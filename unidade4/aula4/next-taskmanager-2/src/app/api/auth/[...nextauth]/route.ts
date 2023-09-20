c64a5f53ed27c113b71f592d7a80b92c959146e8
Daniel Souza <dssouzadan@gmail.com>
2023-09-20 15:37:34 -0300

    fix

--- unidade4/aula4/next-taskmanager-2/src/app/api/auth/[...nextauth]/route.ts
+++ unidade4/aula4/next-taskmanager-2/src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,44 @@
-cd
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+};
+
+const handler =  NextAuth(authOptions);
+export { handler as GET, handler as POST };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
